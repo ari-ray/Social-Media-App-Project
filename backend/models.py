7b236bc5145983e28acf6e14a170648b6fad1295
Arittri Ray <rayarittri01@gmail.com>
2026-03-16 02:04:33 +1100

    feat: updating import

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey, UniqueConstraint
 from sqlalchemy.orm import relationship
 from datetime import datetime, timezone
-from .database import Base
+from backend.database import Base
 
 class User(Base):
     __tablename__ = "users"  # Name of the table in the DB
